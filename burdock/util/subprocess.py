025ab8157731fd231dbc49c5acbac0292d6db3a1
Dylan Lukes <lukes.dylan@gmail.com>
2019-06-18 17:55:45 -0700

    sanely run daikon

--- burdock/util/subprocess.py
+++ burdock/util/subprocess.py
@@ -4,7 +4,7 @@ from shlex import quote
 
 
 def run_daikon(decls: PathLike, dtrace: PathLike):
-    cmd = "java -cp /usr/local/opt/daikon/daikon-src/daikon.jar daikon.Daikon --nohierarchy {decls} {dtrace}".format(decls=quote(decls), dtrace=quote(dtrace))
+    cmd = "java -cp $DAIKONDIR/daikon.jar daikon.Daikon --nohierarchy {decls} {dtrace}".format(decls=quote(decls), dtrace=quote(dtrace))
     stdout = subprocess.check_output(cmd, shell=True)
 
     for line in stdout.strip().decode().splitlines():
